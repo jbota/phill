719205838aaf016f179898aa58fd6b78e156eb52
Joao Bota <jbota@c2r1s3.42lisboa.com>
2022-03-10 17:48:32 +0000

    philo.c

---
+++ philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbota <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/10 17:38:22 by jbota             #+#    #+#             */
+/*   Updated: 2022/03/10 17:38:24 by jbota            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHIL_H
+# define PHIL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+typedef struct s_data {
+	char	**param;
+	int		argc;
+}		t_data;
+
+#endif
