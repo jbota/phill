ac8ee4ae8051ec49d2ffd194bad52a3d370bd44c
Joao Bota <jbota@c2r1s1.42lisboa.com>
2022-03-15 16:24:51 +0000

    ola

--- philo.c
+++ philo.c
@@ -6,11 +6,59 @@
 /*   By: jbota <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:29:34 by jbota             #+#    #+#             */
-/*   Updated: 2022/03/10 17:30:21 by jbota            ###   ########.fr       */
+/*   Updated: 2022/03/15 16:23:13 by jbota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+void	init_mutex_fork(t_data data)
+{
+	int	i;
+
+	i = 0;
+	data.forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	if (!data.nb_forks)
+		printf("Unable to allocate memory\n");
+	while (i++ < data.nb_philo)
+	{
+		if (pthread_mutex_init(&data.forks, NULL))
+			printf("Unabe to start mutex\n");
+	}
+}
+
+void	init_philos(t_data data)
+{
+	int	i;
+
+	i = 0;
+	data->philos = (t_data *)malloc(sizeof(t_data) * data.nb_philo);
+	if (!data->philos)
+		printf("Unable to allocate memory\n");
+	while (i++ < data.nb_philos)
+	{
+		data->philos[i].nph = i;
+		data->philos[i].fork = i;
+		data->philos[i].forkl = (i + 1) % data.nb_philo;
+		data->philos[i].start_t = 0;
+		data->philos[i].n_meals = 0;
+		data->philos[i].data = data;
+		if(pthread_mutex_init(&data.philos[i].mutex, NULL))
+			printf("Unable to start mutex\n");
+	}
+}
+
 int main(int argc, char **argv)
 {
+	t_data	data;
+	int	i;
+
+	memset(&data, 0, sizeof(t_data));
+	if (argc != 5 && argc != 6)
+		printf("Number of arguments are wrong\n");
+	i = 0;
+	read_args(argc, argv, data);
+	init_mutexi_fork(data);
+
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: jbota <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:38:22 by jbota             #+#    #+#             */
-/*   Updated: 2022/03/10 17:38:24 by jbota            ###   ########.fr       */
+/*   Updated: 2022/03/15 16:23:16 by jbota            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,33 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <pthread.h>
 
 typedef struct s_data {
-	char	**param;
-	int		argc;
+
+	int				argc;
+	unsigned long	nb_philo;
+	long			time_die;
+	long			time_eat;
+	long			time_sleep;
+	long			eat_x_times;
+	pthread_mutex_t	*forks;
+	t_data			*philos;
 }		t_data;
+
+typedef struct s_data {
+	int				nph;
+	int				fork;
+	int				forkl;
+	int				start_t;
+	int				n_meals;
+	phtread_t		phil_th;
+	phtread_t		check;
+	pthread_mutex_t	mutex;
+}	t_philos;
+
+void read_args(int argc, char **argv, t_data data);
+void init_mutex_fork(t_dta data);
+void init_philos(t_data data);
 
 #endif
